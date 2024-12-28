159b6b691336995ae095043bff8e335cd3f6ff18
Arkadii <arkady2003@gmail.com>
2024-12-28 10:58:37 +0400

    initial commit. create blank project

---
+++ src/test/java/com/client/mqttclient/MqttclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.mqttclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
